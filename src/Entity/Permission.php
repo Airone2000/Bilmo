f7006ad029c2a662e7f6c01c393444fd9a42320c
ErwanGuillou <contact@erwanguillou.com>
2018-06-02 19:12:19 +0200

    Permissions / HttpNotFound / Swagger doc Close #9

---
+++ src/Entity/Permission.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="permission")
+ * @ApiResource(
+ *   collectionOperations={
+ *      "GET"={
+ *          "controller"=App\Controller\HttpNotFoundAction::class
+ *      }
+ *   },
+ *   itemOperations={
+ *      "GET"={
+ *          "controller"=App\Controller\HttpNotFoundAction::class
+ *      }
+ *   }
+ * )
+ */
+class Permission
+{
+  
+  /**
+   * @var null|string
+   * @ORM\Id()
+   * @ORM\GeneratedValue(strategy="NONE")
+   * @ORM\Column(type="string", length=50)
+   */
+  private $id;
+  
+  /**
+   * @var null|string
+   * @ORM\Column(type="string", nullable=true)
+   */
+  private $description;
+  
+  /**
+   * @return null|string
+   */
+  public function getId(): ?string
+  {
+    return $this->id;
+  }
+  
+  /**
+   * @param string $id
+   * @return \App\Entity\Permission
+   */
+  public function setId(string $id): self
+  {
+    $this->id = $id;
+    return $this;
+  }
+  
+  /**
+   * @return null|string
+   */
+  public function getDescription(): ?string
+  {
+    return $this->description;
+  }
+  
+  /**
+   * @param null|string $description
+   * @return \App\Entity\Permission
+   */
+  public function setDescription(?string $description): self
+  {
+    $this->description = $description;
+    return $this;
+  }
+  
+  
+}
---
+++ src/Swagger/SwaggerDecorator.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace App\Swagger;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class SwaggerDecorator implements NormalizerInterface
+{
+  private $decorated;
+  
+  public function __construct(NormalizerInterface $decorated)
+  {
+    $this->decorated = $decorated;
+  }
+  
+  public function normalize($object, $format = null, array $context = [])
+  {
+    $docs = $this->decorated->normalize($object, $format, $context);
+    
+    // Following routes (actions) are enabled only for IRI generation.
+    // They're definitely not designed to be any useful action and thus
+    // they're not meant to appear in the doc.
+    unset($docs['paths']['/permissions']);
+    unset($docs['paths']['/permissions/{id}']);
+    
+    
+    return $docs;
+  }
+  
+  public function supportsNormalization($data, $format = null)
+  {
+    return $this->decorated->supportsNormalization($data, $format);
+  }
+}
